1e8accae0d7362acf59f325579cfae17cd085f1d
satdoug <satdoug_@hotmail.com>
2018-10-06 17:26:42 -0300

    Subindo o projeto para o GitHub

---
+++ src/main/java/com/produtos/apirest/ApiRestApplication.java
@@ -0,0 +1,16 @@
+package com.produtos.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Criador por satdo no dia 29/09/2018 as 20:45.
+ */
+@SpringBootApplication
+public class ApiRestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiRestApplication.class, args);
+    }
+}
